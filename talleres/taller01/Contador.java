1fe89589b423962331c1947730d9f70dd0543829
mframosg <53051495+mframosg@users.noreply.github.com>
2019-07-18 23:58:02 -0500

    Update Contador.java

--- talleres/taller01/Contador.java
+++ talleres/taller01/Contador.java
@@ -1,23 +1,25 @@
+import java.util.Scanner;
 
 /**
  * La clase Contador tiene la intención de representar un contador.
- * 
- * @author Simon Marin, Miguel Fernando Ramos
- * @version 1
+ *
+ * @author Mauricio Toro, Andres Paez, Daniel Mesa, Miguel Ramos,  Simón Mmarín
+ * @version 2
  */
 
 public class Contador {
-    
-	private int cuenta;
-	private final String id;
+    private int cantidad;
+    private int cuenta;
+    private final String id;
 
 
-	/**
+    /**
      * Se inicializan las variables globales en el constructor de manera que no posean valores nulos o 0s.
      */
-    public Contador(int cuenta, String id) {
-	    this.id = id;
-	    cuenta = 0;
+    public Contador(String id) {
+        this.id = id;
+        cuenta=0;
+        cantidad = 0;
     }
 
     /**
@@ -26,31 +28,40 @@ public class Contador {
      *(opcional: se podria pasar un parametro "cantidad" para incrementar esa cantidad de unidades).
      */
     public void incrementar() {
-	    cuenta += 1;
+        cuenta +=1;
     }
-
-    /*
-    Se podría implementar un metodo decrementar para hacer la operacion inversa de incrementar.
-    */
+    
+    public void descrementar() {
+        cuenta-=1;
+    }
 
     /**
      * El método incrementos nos muestra en que valor se encuentra la cuenta actualmente.
      * @return el numero actual de la cuenta
      */
-    public int incrementos() {
-	    return cuenta;
+
+    public int leerValorIcremento() {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese el valor a incrementar: ");
+        cantidad = teclado.nextInt();
+        return cantidad;
+    }
+    public int leerValorDecremento() {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese el valor a reducir: ");
+        cantidad = teclado.nextInt();
+        return cantidad;
     }
 
-    
-     /**
-    * toString se encargará de convertir el contador en un tipo cadena
-    * para su posterior visualización
-    * se debe de mostrar el estado del contador y su id
-    *
-    * @return una cadena que contiene el id del contador y su cuenta
-    */
+
+    /**
+     * toString se encargará de convertir el contador en un tipo cadena
+     * para su posterior visualización
+     * se debe de mostrar el estado del contador y su id
+     *
+     * @return una cadena que contiene el id del contador y su cuenta
+     */
     public String toString() {
-	    
-	    return this.id+this.cuenta;
+        return " su valor es: "+cuenta;
     }
 }
