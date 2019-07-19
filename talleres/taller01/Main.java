ea0977fed1f204f7e7c9f6acda9d249417ff83c5
mframosg <53051495+mframosg@users.noreply.github.com>
2019-07-19 00:01:02 -0500

    Update Main.java

--- talleres/taller01/Main.java
+++ talleres/taller01/Main.java
@@ -1,22 +1,28 @@
+
 public class Main {
 
     public static void main(String[] args) {
-        Contador c = new Contador(0,"Mauricio");
-        for (int i = 0; i < 10; ++i)
+        int variable;
+        Contador c = new Contador("Mauricio");
+        System.out.println("Iniciamos el contador en 0");
+        variable = c.leerValorIcremento();
+        for (int i = 0; i < variable; ++i) {
             c.incrementar();
-        // 10
-        System.out.println(c);
+        }
+        System.out.println("Hacemos aumento de "+ variable +" al contador,"+ c);
+        variable = c.leerValorDecremento();
+        for (int i = 0; i < variable ; i++) {
+            c.descrementar();
+        }
+        System.out.println("Hacemos una disminución de "+ variable +" al contador,"+ c);
+        System.out.println("\nAl final,"+c);
 
         System.out.println();
 
-        Punto p = new Punto(10, 20);
-        // 0,0
+        Punto p = new Punto(20, 10);
         System.out.printf("Punto: {%f, %f}\n", p.x(), p.y());
-        // 1*sqrt(2)
         System.out.println("Radio Polar: " + p.radioPolar());
-        // -45
         System.out.println("Angulo Polar: " + Math.toDegrees(p.anguloPolar()));
-        // 2
         System.out.println("Angulo Polar: " + p.distanciaEuclidiana(new Punto(0, 0)));
 
         System.out.println();
