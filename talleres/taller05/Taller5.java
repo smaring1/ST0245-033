a2929de37b4e98f9dee76bcfc37fd11f4becc898
smaring1 <53051453+smaring1@users.noreply.github.com>
2019-08-15 11:04:34 -0500

    Create Taller5.java

---
+++ talleres/taller05/Taller5.java
@@ -0,0 +1,46 @@
+/**
+ * La clase Taller5 tiene la intención de resolver el taller número 5.
+ *
+ * @author Mauricio Toro, Andres Paez
+ * @version 1
+ */
+
+public class Taller5 {
+
+
+
+    /**
+     * @param array es una arreglo de numeros enteros.
+     * El método suma tiene la intención de hacer el proceso de suma
+     * mediante una funcion cíclica (while/for/...)
+     * @return la suma de todos los numeros sumados.
+     */
+    public static int suma (int[]array){
+        //...
+    }
+
+
+    /**
+     * @param num es el numero el cual se utiliza para ser multiplicado.
+     * El método mul tiene la intención de hacer la multiplicación
+     * de 1 a n por el numero mul
+     * mediante una funcion cíclica (while/for/...)
+     *
+     */
+    public static void mul (int num){
+        //...
+    }
+
+
+    /**
+     * @param array es un arreglo de números desordenados
+     * El método insertionSort tiene la intención ordenar los números
+     * del arreglo array por el método insertion:
+     * @see <a href="https://www.youtube.com/watch?v=OGzPmgsI-pQ"> Insertion Sort <a/>
+     * mediante la anidación de funciones cíclicas (while/for/...)
+     *
+     */
+    public static int[] insertionSort (int[] array){
+        //...
+    }
+}
