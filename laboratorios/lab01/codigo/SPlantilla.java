ad60af286b959dbf567508a0e6942324d5f0b7e0
mframosg <53051495+mframosg@users.noreply.github.com>
2019-08-17 17:06:27 -0500

    Update SPlantilla.java

--- laboratorios/lab01/codigo/SPlantilla.java
+++ laboratorios/lab01/codigo/SPlantilla.java
@@ -1,7 +1,7 @@
 /**
  * La siguiente clase contiene la solucion al punto #1 del
  * Laboratorio #1 del curso de estructura de datos y algoritmos 1
- * @author Mauricio Toro, Daniel Mesa, Simón Marín, Miguel Ramos
+ * @author Mauricio Toro, Daniel Mesa
  * @version 1.0
  */
 public class SPlantilla
@@ -13,11 +13,15 @@ public class SPlantilla
      *           de ubicar el rectangulo 1x2
      * @return int retorna un entero que representa las formas en las que se puede llenar el rectangulo 2xn
      */
+    /**
+     *  C_2 + T(n-1) + T(n-2)
+     */
     public static int nRectangulos(int n)
     {
-        if (n <= 2) return n;
-        else return nRectangulos(n - 1) + nRectangulos(n - 2);
+        if (n <= 2) return n;   // C_1
+        else return nRectangulos(n - 1) + nRectangulos(n - 2); //T(n) = T(n-1) + T(n-2)
     }
+
 
     /**
      * Metodo principal, el cual ejecuta el programa e imprime el resultado
