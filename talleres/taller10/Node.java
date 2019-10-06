f7464f6991c59b156376dcdfac8817858e69fcbe
mframosg <53051495+mframosg@users.noreply.github.com>
2019-10-06 12:45:37 -0500

    Create Node.java

---
+++ talleres/taller10/Node.java
@@ -0,0 +1,20 @@
+
+public class Node {
+ public Node left;
+ public Node right;
+ public int data;
+ public Node(int d){
+	 
+	data = d;
+ }
+  public Node getLeft(){
+        return this.left;
+    }
+    public Node getRigth(){
+        return this.right;
+    }
+    public int getData(){
+        return this.data;
+    }
+   }
+}
